11ed4fc2bd79e7ec14a0bf765459cb56866a9718
Tariq Zeyad Sharaia <tariq.z.sharaia@gmail.com>
2021-09-14 18:08:51 +0300

    carousel added

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,18 +2,57 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import { withAuth0 } from "@auth0/auth0-react"
+import axios from 'axios';
+import Bookitem from './Bookitem';
 
 class MyFavoriteBooks extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      favBooksArr: [],
+    }
+
+  }
+
+  // to render the data directly
+  componentDidMount = () => {
+    const { user } = this.props.auth0;
+    const email = user.email;
+    console.log(email);
+    axios
+      .get(`http://localhost:3010/getbooks?email=${email}`)
+      .then(result => {
+        console.log(result.data);
+        this.setState({
+          favBooksArr: result.data
+        })
+      })
+      .catch(err => {
+        console.log('error');
+      })
+  }
+
   render() {
-    return(
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
         <p>
           This is a collection of my favorite books
         </p>
+        {this.state.favBooksArr.map(item => {
+          return (
+            <Bookitem
+              item={item}
+            />
+
+          )
+        })
+        }
       </Jumbotron>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
